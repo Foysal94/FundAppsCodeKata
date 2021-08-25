dcacd040ba126aa0fcd60331d072c966a89a26e2
Foysal Ahmed <Foysal94@Outlook.com>
2021-08-25 20:28:15 +0100

    Basic code to read the number of parcels and each parcel size

--- src/FundAppsCodeKata.Main/Program.cs
+++ src/FundAppsCodeKata.Main/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FundAppsCodeKata.Main
 {
@@ -6,7 +7,16 @@ namespace FundAppsCodeKata.Main
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("How many parcels?");
+            var parcelCount = int.Parse(Console.ReadLine());
+
+            IList<int> parcelSizeLists = new List<int>();
+
+            for(int i = 1; i < parcelCount + 1; i++)
+            {
+                Console.Write($"What is the size of parcel {i}:");
+                parcelSizeLists.Add(int.Parse(Console.ReadLine()));
+            }
         }
     }
 }
