c99f8d9287c9b0680628e54c7d8048b6ba444fd8
Foysal Ahmed <Foysal94@Outlook.com>
2021-08-25 22:32:17 +0100

    Add to output cost of with and without speedy shipping

--- src/FundAppsCodeKata.Main/Program.cs
+++ src/FundAppsCodeKata.Main/Program.cs
@@ -12,7 +12,7 @@ namespace FundAppsCodeKata.Main
             Console.Write("How many parcels: ");
             var parcelCount = int.Parse(Console.ReadLine());
 
-            IList<int> parcelSizes = new List<int>();
+            List<int> parcelSizes = new List<int>();
 
             for (int i = 1; i < parcelCount + 1; i++)
             {
@@ -20,17 +20,15 @@ namespace FundAppsCodeKata.Main
                 parcelSizes.Add(int.Parse(Console.ReadLine()));
             }
 
-            double totalCost = 0;
             var parcelShippingCalculatorService = new ParcelShippingCalculatorService();
-            foreach (var parcelSize in parcelSizes)
-            {
-                totalCost = totalCost + parcelShippingCalculatorService.Calculcate(parcelSize);
-            }
+            var orderService = new OrderService(parcelShippingCalculatorService);
+
+            double totalCostWithoutSpeedyShipping = orderService.CalculateOrder(parcelSizes, false);
+            double totalCostWithSpeedyShipping = orderService.CalculateOrder(parcelSizes, true);
 
             const string parcelOuputDetails = "{0} parcel count: {1}, each costing ${2}";
 
             // Maybe move this to another service to unit test the outputted count is correct?
-            Console.WriteLine($"Total Cost: ${totalCost}");
             Console.WriteLine(string.Format(parcelOuputDetails, "Small", parcelSizes.Count(size => size <= Constants.MAXIMUM_SMALL_PARCEL_SIZE), Constants.SMALL_PARCEL_SIZE_COST));
             Console.WriteLine(string.Format(parcelOuputDetails, "Medium", 
                 parcelSizes.Count(size => size >= Constants.MINIMUM_MEDIUM_PARCEL_SIZE && size <= Constants.MAXIMUM_MEDIUM_PARCEL_SIZE),
@@ -41,6 +39,10 @@ namespace FundAppsCodeKata.Main
             Console.WriteLine(string.Format(parcelOuputDetails, "Extra Large",
                 parcelSizes.Count(size => size >= Constants.MINIMUM_EXTRA_LARGE_PARCEL_SIZE),
                 Constants.EXTRA_LARGE_PARCEL_SIZE_COST));
+
+            Console.WriteLine($"Total Cost without SpeedyShipping: ${totalCostWithoutSpeedyShipping}");
+
+            Console.WriteLine($"Total Cost with SpeedyShipping: ${totalCostWithSpeedyShipping}");
 
             Console.ReadLine();
         }
