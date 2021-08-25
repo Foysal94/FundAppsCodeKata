aedeb8674c352057232fc2948780b1ab27573bf6
Foysal Ahmed <Foysal94@Outlook.com>
2021-08-25 21:28:28 +0100

    Add Constants file to keep track of parcel sizes.

---
+++ src/FundAppsCodeKata.Main/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FundAppsCodeKata.Main
+{
+    public static class Constants
+    {
+        public const int MINIMUM_SMALL_PARCEL_SIZE = 1;
+        public const int MAXIMUM_SMALL_PARCEL_SIZE = 9;
+        public const int MINIMUM_MEDIUM_PARCEL_SIZE = 10;
+        public const int MAXIMUM_MEDIUM_PARCEL_SIZE = 49;
+        public const int MINIMUM_LARGE_PARCEL_SIZE = 50;
+        public const int MAXIMUM_LARGE_PARCEL_SIZE = 99;
+        public const int MINIMUM_EXTRA_LARGE_PARCEL_SIZE = 100;
+    }
+}
